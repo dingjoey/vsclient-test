2ad82980c3036a69291c45c8b7bed953afbbc169
qiaoyi.dingqy_055920 <qiaoyi.dingqy@f2ehz3.sqa.cm6>
2013-06-25 13:39:40 +0800

    test sample code

--- test1_test.c
+++ test1_test.c
@@ -3,5 +3,12 @@
 
 TEST(TESTSUITENAME, TESTCASENAME)
 {
+    VSResult result;
+    const char *domain = "youhui.etao.com";
+    select_ip(domain, &result);
+    printf("Choose: %s\n", result.ip_str);
+    printf("Show Success Message: %d\n", result.is_success);
+    printf("Show Error Message: %s\n", result.er_msg);
+
     EXPECT_EQ(1,1);
 }
